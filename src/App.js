cf4183e3069dae49057daa037392bc467db70f75
Vinicius Crispim <vinicrispim02@hotmail.com>
2023-07-25 17:48:28 -0300

    Elemento campo de texto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Banner from './components/Banner/Banner';
+import CampoTexto from './components/CampoTexto/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      
+      <CampoTexto/>
     </div>
   );
 }
---
+++ src/components/CampoTexto/CampoTexto.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+    return (
+        <div className='campo-texto'>
+            <label>Nome</label>
+            <input type="text" />
+        </div>
+    )
+}
+
+export default CampoTexto;
